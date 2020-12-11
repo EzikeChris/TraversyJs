7126c60db28f499c4aa9d5eb4f0ef5feb4b5d6e2
ezikechris <christophermbe1009@gmail.com>
2020-12-11 11:54:10 +0100

    initial commit

---
+++ Arrow.js
@@ -0,0 +1,10 @@
+//// ARROW FUNCTIONS /// 
+
+const figmaUser = (userName, userEmail, userHobby) => {
+    return (
+        'name is' + userName + 'email is ' + userEmail + 'this is hobby' + userHobby
+    );
+
+};
+
+console.log(figmaUser());
